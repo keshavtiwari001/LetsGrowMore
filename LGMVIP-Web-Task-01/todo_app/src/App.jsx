aa75532959329ef315b7262154c6300130f5ac7b
keshavtiwari001 <keshavtiwari.dev@gmail.com>
2024-06-17 21:42:25 +0530

    todo app started

---
+++ LGMVIP-Web-Task-01/todo_app/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Todoinput from './components/Todoinput'  
+
+function App() {
+return(
+      <>
+      <div className='main-container'></div>
+      <div className='center-container'>
+            <Todoinput/>
+      </div>
+</>
+     
+)
+}
+
+export default App
---
+++ LGMVIP-Web-Task-01/todo_app/src/components/Todoinput.jsx
@@ -0,0 +1,11 @@
+
+const Todoinput = () => {
+  return (
+    <div className="input-container">
+            <input type="text" className="input-box-todo" placeholder="Enter your task" />
+            <button className="add-btn">+</button>
+    </div>
+  )
+}
+
+export default Todoinput
