4e7671c84ed6104f4c40ef36ec162e836f32cd57
keshavtiwari001 <keshavtiwari.dev@gmail.com>
2024-06-18 22:56:19 +0530

    some js added

--- LGMVIP-Web-Task-01/todo_app/src/App.jsx
+++ LGMVIP-Web-Task-01/todo_app/src/App.jsx
@@ -1,12 +1,15 @@
+import { useState } from 'react'
 import './App.css'
-import Todoinput from './components/Todoinput'  
+import Todoinput from './components/Todoinput'
 
 function App() {
+      const [listTodo,setListTodo]= useState([]);
+      let addList = (inputText)=>{setListTodo([...listTodo,inputText])};
 return(
       <>
       <div className='main-container'></div>
       <div className='center-container'>
-            <Todoinput/>
+            <Todoinput addList={addList}/>
       </div>
 </>
      
--- LGMVIP-Web-Task-01/todo_app/src/components/Todoinput.jsx
+++ LGMVIP-Web-Task-01/todo_app/src/components/Todoinput.jsx
@@ -1,9 +1,12 @@
+import React,{ useState } from "react";
 
-const Todoinput = () => {
+function Todoinput(props) {
+  const [inputText, setInputText] = useState('');
   return (
     <div className="input-container">
-            <input type="text" className="input-box-todo" placeholder="Enter your task" />
-            <button className="add-btn">+</button>
+            <input type="text" className="input-box-todo" placeholder="Enter your task" value={inputText} onChange={e=>{setInputText(e.target.value)}}/>
+            <button className="add-btn" onClick={()=>{props.addList(inputText); setInputText('')}}>+</button>
+            
     </div>
   )
 }
