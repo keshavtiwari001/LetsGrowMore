d6195f8324a3c47a519e4f3f54ffb2aaeb431d56
keshavtiwari001 <keshavtiwari.dev@gmail.com>
2024-07-11 11:42:56 +0530

    task 1 complete

--- LGMVIP-Web-Task-01/todo_app/src/App.jsx
+++ LGMVIP-Web-Task-01/todo_app/src/App.jsx
@@ -1,15 +1,33 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import Todoinput from './components/Todoinput'
+import TodoList from './components/TodoList';
 
 function App() {
       const [listTodo,setListTodo]= useState([]);
-      let addList = (inputText)=>{setListTodo([...listTodo,inputText])};
+      let addList = (inputText)=>{
+            if(inputText!== ''){
+                  setListTodo([...listTodo,inputText])
+            }
+            };
+
+      const deleteListItem = (key)=>{
+            let newListTodo = [...listTodo];
+            newListTodo.splice(key,1)
+            setListTodo([...newListTodo]);
+      }
 return(
       <>
       <div className='main-container'></div>
       <div className='center-container'>
             <Todoinput addList={addList}/>
+            <h1 className="app-heading">Todo</h1>
+            <hr />
+            {listTodo.map((listItem,i)=>{
+                  return(
+                        <TodoList  key={i} index={i} item={listItem} deleteItem={deleteListItem}/>
+                  )
+            })}
       </div>
 </>
      
